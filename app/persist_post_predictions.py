47c8a9a3856dc08a90089372180b007f45f309e2
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-04-02 01:41:03 -0300

    lets give it a try

--- app/persist_post_predictions.py
+++ app/persist_post_predictions.py
@@ -7,6 +7,7 @@ import shutil
 from json.decoder import JSONDecodeError
 import logging
 
+
 def clean_json_file():
     data = {
         "plate_pred": [
@@ -22,70 +23,123 @@ def clean_json_file():
                 "img_height": "",
                 "processing_time_ms": "",
                 "regions_of_interest": [],
-                "results": []
-                }
-            ]
-        }
-    with open('/json_source/db.json', 'w') as f:
+                "results": [],
+            }
+        ]
+    }
+    with open("/json_source/db.json", "w") as f:
         json.dump(data, f)
-def main():
-    server_url = 'http://json_server/plate_pred'
-    category = 'placa_carro'
-    detect_dir = Path('/detect')
-    old_det_dir = detect_dir / 'old'
-    while not [item for item in detect_dir.glob('*') if not os.path.samefile(item, old_det_dir) and not os.path.commonpath([item, old_det_dir]) == old_det_dir]:
-        time.sleep(0.5)
-    latest_detection = sorted([item for item in detect_dir.glob('*') if not os.path.samefile(item, old_det_dir) and not os.path.commonpath([item, old_det_dir]) == old_det_dir], key=lambda x: x.stat().st_mtime, reverse=True)[0].name
-    pred_files_dir = Path('/logs') / latest_detection / 'processed_plates' / category
-    logs_dir = Path('/logs') / latest_detection / 'posted_plates' / category
-    os.makedirs(logs_dir, exist_ok=True)
 
-    while not os.path.exists(pred_files_dir):
+
+def main():
+    server_url = "http://json_server/plate_pred"
+    categories = [
+        "placa_carro",
+        "placa_carro_mercosul",
+        "placa_moto",
+        "placa_moto_mercosul",
+    ]
+    detect_dir = Path("/detect")
+    old_det_dir = detect_dir / "old"
+    while not [
+        item
+        for item in detect_dir.glob("*")
+        if not os.path.samefile(item, old_det_dir)
+        and not os.path.commonpath([item, old_det_dir]) == old_det_dir
+    ]:
         time.sleep(0.5)
-        
-    id=1
+    latest_detection = sorted(
+        [
+            item
+            for item in detect_dir.glob("*")
+            if not os.path.samefile(item, old_det_dir)
+            and not os.path.commonpath([item, old_det_dir]) == old_det_dir
+        ],
+        key=lambda x: x.stat().st_mtime,
+        reverse=True,
+    )[0].name
+
+    pred_files_dir_placa_carro = (
+        Path("/logs") / latest_detection / "processed_plates" / categories[0]
+    )
+
+    for category in categories:
+        os.makedirs(
+            Path("/logs") / latest_detection / "posted_plates" / category, exist_ok=True
+        )
+
+    while not os.path.exists(pred_files_dir_placa_carro):
+        time.sleep(0.5)
+
+    id = 1
     while True:
-        # Get a list of all files in the log directory
-        log_files = [f for f in os.listdir(pred_files_dir) if f.endswith('.log')]
-        #logging.info(log_files)
-        for log_file in sorted(log_files):
-            
-            log_path = Path(pred_files_dir) / log_file
-            #logging.info(log_path)
-            
-            # Read the contents of the log file as a JSON object
-            try:
-                with open(log_path) as f:
-                    log_data = json.load(f)
-            except JSONDecodeError:
-                #logging.info("Invalid JSON data in log file, continuing...")
-                continue
-            #logging.info(log_data['results'])
-            if not log_data['results']:
-                os.remove(log_path)
-                #logging.info("Empty prediction, continuing...")
-                continue
-            # Add the filename to the JSON object
-            log_data["id"] = id
-            log_data["file"] = log_file
-            log_data["category"] = category
-            
-            # Post the JSON object to the server
-            response = requests.post(server_url, json=log_data)
-            #logging.info("response:",response)
-            while not response.ok:
-                # Move the file to the posted directory if the post was successful
-                response = requests.post(server_url, json=log_data)
-                #logging.info("response:",response)
-            id += 1  
-            shutil.move(log_path, logs_dir / log_file)
-        else:
-            # Wait for some time before checking for new files again
+        logs_dict = {
+            "placa_carro": [],
+            "placa_carro_mercosul": [],
+            "placa_moto": [],
+            "placa_moto_mercosul": [],
+        }
+        for category in categories:
+            # Get a list of all files in the log directory
+            logs_dict[category] = sorted(
+                [
+                    f
+                    for f in os.listdir(
+                        Path("/logs") / latest_detection / "processed_plates" / category
+                    )
+                    if f.endswith(".log")
+                ]
+            )
+        # logging.info(log_files)
+        if not any(logs_dict.values()):
             time.sleep(0.5)
-            new_files = sorted(os.listdir(pred_files_dir))
-            if new_files != log_files:
-                    #logging.info("New files detected. Processing...")
-                    log_files = new_files
-if __name__ == '__main__':
+            continue
+        for category, log_file_list in logs_dict.items():
+            for log_file in log_file_list:
+                log_path = (
+                    Path("/logs")
+                    / latest_detection
+                    / "processed_plates"
+                    / category
+                    / log_file
+                )
+                # logging.info(log_path)
+
+                # Read the contents of the log file as a JSON object
+                try:
+                    with open(log_path) as f:
+                        log_data = json.load(f)
+                except JSONDecodeError:
+                    # logging.info("Invalid JSON data in log file, continuing...")
+                    continue
+                # logging.info(log_data['results'])
+                if not log_data["results"]:
+                    os.remove(log_path)
+                    # logging.info("Empty prediction, continuing...")
+                    continue
+                # Add the filename to the JSON object
+                log_data["id"] = id
+                log_data["file"] = log_file
+                log_data["category"] = category
+
+                # Post the JSON object to the server
+                response = requests.post(server_url, json=log_data)
+                # logging.info("response:",response)
+                while not response.ok:
+                    # Move the file to the posted directory if the post was successful
+                    response = requests.post(server_url, json=log_data)
+                    # logging.info("response:",response)
+                id += 1
+                shutil.move(
+                    log_path,
+                    Path("/logs")
+                    / latest_detection
+                    / "posted_plates"
+                    / category
+                    / log_file,
+                )
+
+
+if __name__ == "__main__":
     clean_json_file()
     main()
